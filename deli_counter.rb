4ceee02a552fd9fbadc074561dc60a3d9f963fe4
Romano Livi <romano_livi@outlook.com>
2020-12-12 17:18:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,8 @@ end
 def now_serving(katz_deli)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
+  else puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
   end
-  puts "Currently serving #{katz_deli[0]}"
-  katz_deli.shift
 end
 

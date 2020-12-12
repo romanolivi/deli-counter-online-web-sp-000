e2e29f22998f39a586e512dc1a3718a1f873f0b9
Romano Livi <romano_livi@outlook.com>
2020-12-12 17:36:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
-    message = "The line is currently: "
+    message = "The line is currently:"
     
     katz_deli.each_with_index do |value, index|
       message += " #{index.to_i + 1}. #{value}"

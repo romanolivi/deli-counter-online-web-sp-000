16c2c450f4bfed28f0bc4a5029dbcc134f03e816
Romano Livi <romano_livi@outlook.com>
2020-12-12 17:27:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,10 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently; #{katz_deli}"
+    message = "The line is currently: "
+    
+    katz_deli.each_with_index do |value, index|
+      message += (index + 1. value)
   end
 end
 

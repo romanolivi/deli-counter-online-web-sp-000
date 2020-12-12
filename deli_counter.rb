523694080f4700142711ebac4ad1bebc9b14ba9f
Romano Livi <romano_livi@outlook.com>
2020-12-12 17:29:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     message = "The line is currently: "
     
     katz_deli.each_with_index do |value, index|
-      message += (index + 1.)
+      message += "#{index.to_i + 1}. #{value}"
   end
 end
 
